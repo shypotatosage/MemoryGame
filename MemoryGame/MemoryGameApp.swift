7428e315ca44f1d89ce75c56f247c3440c81a4eb
Mimi <91705093+shypotatosage@users.noreply.github.com>
2023-04-18 19:22:13 +0700

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by MacBook Pro on 18/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
